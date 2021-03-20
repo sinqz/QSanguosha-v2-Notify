fe97448ed696e96712969da006ca5aced2529419
Notify-ctrl <70358032+Notify-ctrl@users.noreply.github.com>
2021-03-20 15:12:36 +0000

    复制神杀的文件

---
+++ src/pch.h
@@ -0,0 +1,30 @@
+﻿#ifndef PCH_H
+#define PCH_H
+
+#ifdef _MSC_VER
+#pragma execution_character_set("utf-8")
+#endif
+
+//#define LOGNETWORK
+
+#include <ft2build.h>
+
+#ifdef __cplusplus
+
+#include <QtCore>
+#include <QtNetwork>
+#include <QtGui>
+#include <QtWidgets>
+
+#ifndef Q_OS_WINRT
+#include <QtQml>
+#endif
+
+#ifdef AUDIO_SUPPORT
+#include <fmod.hpp>
+#endif
+
+#endif
+
+#endif // PCH_H
+
